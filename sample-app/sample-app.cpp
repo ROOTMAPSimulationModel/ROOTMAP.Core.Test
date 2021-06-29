5008034170e6eef7bf3073e72ebde3266d8ea553
Mike Airey <mike.s.airey@gmail.com>
2021-06-29 21:35:14 +1000

    Updated nuget dependencies to latest.

--- sample-app/sample-app.cpp
+++ sample-app/sample-app.cpp
@@ -1,5 +1,5 @@
-// sample-app.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// This is an extremely barebones sample application. Its sole purpose is to embed the ROOTMAP.Core library in a runnable C++ application
+// to ensure the library passes the most basic 'does it work' test.
 
 #include "pch.h"
 #include <iostream>
@@ -10,14 +10,3 @@ int main()
     std::cout << "ROOTMAP.Core sample client application." << std::endl;
     std::cout << "Library version: " << rootmapCoreVersion << std::endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
